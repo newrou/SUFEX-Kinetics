563c35b04d5c65048e3af4aab90c6ea5ac96b145
Alex Root <newrou@mail.ru>
2021-02-25 15:28:03 +0700

    Integrate MS Script

--- MS/Integrate/Integrate-All-MS-BSF.sh
+++ MS/Integrate/Integrate-All-MS-BSF.sh
@@ -12,7 +12,8 @@ do
   FileConverter -in $name-MNa.mzML -out Int-MS/$name-MNa.dta2d
   FileFilter -in tmp-ms.mzML -out $name-MK.mzML -int 100: -rt 30:1030 -mz 272.98:273.02
   FileConverter -in $name-MK.mzML -out Int-MS/$name-MK.dta2d
-
+  ./Sum-Exp-MS-dta2d.py Int-MS/$name-MH.dta2d Int-MS/$name-MNa.dta2d Int-MS/$name-MK.dta2d | tee Int-MS/$name.sum
   rm tmp-ms.mzML $name-*.mzML
 done
+
 
